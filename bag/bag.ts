42e918d7f1ac89ac8f5eebf844bcc228b69b939f
Farrukh Normuradov <farrukh.normuradov@gmail.com>
2023-08-29 23:05:23 +0200

    feat: bag

---
+++ bag/bag.ts
@@ -0,0 +1,34 @@
+class BagNode<T> {
+  constructor(public item: T,public next: BagNode<T> | null) {}
+}
+
+export class Bag<T> {
+  private first: BagNode<T>
+
+  add(item: T): void {
+    const oldFirst = this.first
+    this.first = new BagNode(item, oldFirst)
+  }
+
+  private *iterator(): Iterator<T> {
+    let node = this.first
+    while(node) {
+      yield node.item
+      node = node.next
+    }
+  }
+
+  [Symbol.iterator]() {
+    return this.iterator();
+  }
+}
+
+const bag = new Bag<number>()
+bag.add(1)
+bag.add(2)
+bag.add(3)
+
+for(let n of bag) {
+  console.log(n)
+}
+
